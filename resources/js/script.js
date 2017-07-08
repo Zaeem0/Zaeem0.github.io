82eae9b0356ed9e12e861827eaa892a76e69ed89
Zaeem <zaeem.q97@gmail.com>
2017-07-08 15:46:28 +0100

    test change

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    
+
+
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -13,11 +14,100 @@ $(document).ready(function() {
     $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
-    
+
     $('.js--scroll-to-start').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
-    
-    
-    
+
+/*----------------------------------*/
+    $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+/*----------------------------------*/
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+        offset:'50%'
+    })
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset:'50%'
+    })
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{
+        offset:'50%'
+    })
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    },{
+        offset:'50%'
+    })
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+            nav.slideToggle(200);
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+            nav.slideToggle(200);
+        }
+
+    });
+
+
+        var offset = 250;
+
+        var duration = 300;
+
+        $(window).scroll(function() {
+
+        if ($(this).scrollTop() > offset) {
+
+        $('.back-to-top').fadeIn(duration);
+
+        } else {
+
+        $('.back-to-top').fadeOut(duration);
+
+        }
+
+        });
+
+
+
+        $('.back-to-top').click(function(event) {
+
+        event.preventDefault();
+
+        $('html, body').animate({scrollTop: 0}, duration);
+
+        return false;
+
+        })
+
+
 });
